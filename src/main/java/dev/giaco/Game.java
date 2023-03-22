63d12f8c8a4b3a3e9995b24907e74cb7204817cd
pilpod <giacomo.corso@gmail.com>
2023-03-22 11:57:38 +0100

    test game can set input type string, created getters and setters and methode check in class game

--- src/main/java/dev/giaco/Game.java
+++ src/main/java/dev/giaco/Game.java
@@ -2,4 +2,21 @@ package dev.giaco;
 
 public class Game {
     
+    private String input;
+
+    public Game() {
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public Boolean check() {
+        return null;
+    }
+
 }
--- src/test/java/dev/giaco/GameTest.java
+++ src/test/java/dev/giaco/GameTest.java
@@ -1,12 +1,32 @@
 package dev.giaco;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
+    private static Game game;
+
+    @BeforeAll
+    static void beforeClass() {
+        game = new Game();
+    }
+
     @Test
-    public void testExample() {
-        
+    public void testGameReceiveInputTypeString() {
+        game.setInput("0 h 2 3 5 1 v 3 1 1");
+        assertThat(game.getInput(), equalTo("0 h 2 3 5 1 v 3 1 1"));
+    }
+
+    @Test
+    public void testGameBlocksOverlaps() {
+        game.setInput("0 h 2 3 5 1 v 4 1 5");
+        Boolean areOverlaps = game.check();
+        assertThat(areOverlaps, is(true));
     }
 
 }
