aa8dfe15112cd8904431f77c46bce4e803d4b0ec
pilpod <giacomo.corso@gmail.com>
2023-04-03 17:57:51 +0200

    removed import in class Game

--- src/main/java/dev/giaco/Game.java
+++ src/main/java/dev/giaco/Game.java
@@ -3,7 +3,6 @@ package dev.giaco;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import dev.giaco.entities.AbstractBlock;
